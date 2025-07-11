21ef306cb1946897f98906fc2ec9a3a6f58ad87c
krishna07fts <m.krishna@faaztechsolutions.com>
2025-07-11 17:10:23 +0530

    configure changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,11 +27,6 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-
-    viewport: null,
-    launchOptions: {
-      args: ['--window-size=1920,1080'],
-    },
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
